1dbbabfc6a603b210d7790fde173a8270f4767d3
Nataliya Trufanova <nataliya.truffez@gmail.com>
2024-06-13 13:04:56 +0200

    Test: Add snapshot test for TrackersViewController in dark mode

--- TrackerTests/TrackerTests.swift
+++ TrackerTests/TrackerTests.swift
@@ -27,4 +27,20 @@ final class TrackerTests: XCTestCase {
         assertSnapshot(of: vc, as: .image(traits: UITraitCollection(userInterfaceStyle: .light)), record: record)
     }
     
+    // Добавляем тест для темной темы
+    func testTrackersViewControllerDark() {
+        let viewModel = TrackersViewModel()
+        viewModel.categories = [
+            TrackerCategory(title: "Test Category", trackers: [
+                Tracker(id: UUID(), name: "Теннис", color: "colorSelection5", emodji: "🎾", schedule: ReccuringSchedule(recurringDays: [Weekday.thursday.rawValue]), creationDate: Date(), originalCategory: nil)
+            ])
+        ]
+        viewModel.filteredCategories = viewModel.categories
+        
+        let vc = TrackersViewController()
+        vc.viewModel = viewModel
+        
+        let record = false
+        assertSnapshot(of: vc, as: .image(traits: UITraitCollection(userInterfaceStyle: .dark)), record: record)
+    }
 }
